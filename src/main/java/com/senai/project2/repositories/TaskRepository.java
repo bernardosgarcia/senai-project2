8a74c0cf3ba5cfcefa05740732f985f3e9c0d3d6
Bernardo de Sousa Garcia <bernardo.sousa.garcia@gmail.com>
2025-04-25 11:24:22 -0300

    Last Changes

--- src/main/java/com/senai/project2/repositories/TaskRepository.java
+++ src/main/java/com/senai/project2/repositories/TaskRepository.java
@@ -2,6 +2,8 @@ package com.senai.project2.repositories;
 
 import com.senai.project2.models.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<TaskModel, Integer> {
 }
